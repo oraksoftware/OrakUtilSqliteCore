b26c6a9d9b7a9fe6be6737b0e990df98283cd0a7
TuncayHp2 <tunc270@gmail.com>
2026-05-24 20:49:55 +0300

    up and post request eklendi (orak-backup-webs)

--- FiDbHelper/FiSqlite.cs
+++ FiDbHelper/FiSqlite.cs
@@ -1,210 +1,206 @@
-﻿using OrakUtilDotNetCore.FiConfig;
+﻿namespace OrakUtilSqliteCore.FiDbHelper;
+
+using OrakUtilDotNetCore.FiConfig;
 using OrakUtilDotNetCore.FiContainer;
 using OrakUtilDotNetCore.FiOrm;
-
-namespace OrakUtilSqliteCore.FiDbHelper
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+public class FiSqLite
 {
-  //using OrakUtilDotNetFrm.AdoNetHelper;
-  using System;
-  using System.Collections.Generic;
-  using System.Data;
-  using System.Data.SQLite;
+  private string txConnString { get; set; } // private set vardı
 
-
-  public class FiSqLite
+  public static FiSqLite BuiWitProfile(string connProfile)
   {
-    private string txConnString { get; set; } // private set vardı
+    string prTxConnString = FiAppConfig.GetConnStringWthTest(connProfile);
+    return new FiSqLite(prTxConnString);
+  }
 
-    public static FiSqLite BuiWitProfile(string connProfile)
+
+  /**
+   *
+   */
+  public FiSqLite(string prTxConnString)
+  {
+    //string connstr = @"Data Source=Y:\sqlite-data\demo.db";
+    this.txConnString = prTxConnString;
+    //EnsureDatabaseExists();
+  }
+  /**
+   * Database yoksa oluşturulur.
+   */
+  public Fdr EnsureDatabaseExists()
+  {
+    Fdr fdrMain = new Fdr();
+
+    try
     {
-      string prTxConnString = FiAppConfig.GetConnStringWthTest(connProfile);
-      return new FiSqLite(prTxConnString);
-    }
+      // Veritabanı dosyasını kontrol et.
+      var connStringBuilder = new SQLiteConnectionStringBuilder(txConnString);
+      string databaseFile = connStringBuilder.DataSource;
 
-
-    /**
-     *
-     */
-    public FiSqLite(string prTxConnString)
-    {
-      //string connstr = @"Data Source=Y:\sqlite-data\demo.db";
-      this.txConnString = prTxConnString;
-      //EnsureDatabaseExists();
-    }
-    /**
-     * Database yoksa oluşturulur.
-     */
-    public Fdr EnsureDatabaseExists()
-    {
-      Fdr fdrMain = new Fdr();
-
-      try
+      if (!System.IO.File.Exists(databaseFile))
       {
-        // Veritabanı dosyasını kontrol et.
-        var connStringBuilder = new SQLiteConnectionStringBuilder(txConnString);
-        string databaseFile = connStringBuilder.DataSource;
-
-        if (!System.IO.File.Exists(databaseFile))
-        {
-          // Eğer dosya yoksa bir bağlantı açılarak yeni bir veritabanı oluşturulur.
-          SQLiteConnection.CreateFile(databaseFile);
-          fdrMain.SetBoExecAndResultTrue();
-          FiAppConfig.fiLog?.Debug($"Veritabanı oluşturuldu: {databaseFile}");
-          return fdrMain;
-        }
-        // Execution yapılmadı
-        fdrMain.boResult = true;
-        fdrMain.txMessage = "Veritabanı Mevcut.";
-        FiAppConfig.fiLog?.Debug($"Veritabanı mevcut");
-        return fdrMain;
-      }
-      catch (Exception ex)
-      {
-        fdrMain.SetBoExecAndResultFalse();
-        FiAppConfig.fiLog?.Error($"Veritabanı kontrol/oluşturma sırasında bir hata oluştu: {ex.Message}");
-        return fdrMain;
-      }
-
-    }
-
-    public Fdr ExecNonQuery(string query)
-    {
-      FiQuery fiQuery = new FiQuery(query);
-      return ExecNonQuery(fiQuery);
-    }
-
-    public Fdr SqlUpdateQuery(FiQuery fiQuery)
-    {
-      return ExecNonQuery(fiQuery);
-    }
-
-    public Fdr SqlInsertQuery(FiQuery fiQuery)
-    {
-      return ExecNonQuery(fiQuery);
-    }
-
-    public Fdr SqlDeleteQuery(FiQuery fiQuery)
-    {
-      return ExecNonQuery(fiQuery);
-    }
-
-    public Fdr ExecNonQuery(FiQuery fiQuery) //, params ParamItem[] parameters
-    {
-      Fdr fdrMain = new Fdr();
-
-      using SQLiteConnection conn = new SQLiteConnection(this.txConnString);
-      FiAppConfig.fiLog?.Debug(txConnString);
-
-      using SQLiteCommand sqliteCmd = conn.CreateCommand();
-      //Comm.Parameters.Clear();
-      string query = FiQueryUtils.FixSqlProblems(fiQuery.sql);
-      sqliteCmd.CommandText = query;
-      //Comm.CommandType = CommandType.Text;
-
-      try
-      {
-        conn.Open();
-        AddSqlParametersToComm(fiQuery.fkbParams, sqliteCmd);
-        int lnRowsAffected = sqliteCmd.ExecuteNonQuery();
+        // Eğer dosya yoksa bir bağlantı açılarak yeni bir veritabanı oluşturulur.
+        SQLiteConnection.CreateFile(databaseFile);
         fdrMain.SetBoExecAndResultTrue();
-        fdrMain.lnRowsAffected = lnRowsAffected;
+        FiAppConfig.fiLog?.Debug($"Veritabanı oluşturuldu: {databaseFile}");
         return fdrMain;
       }
-      catch (Exception e)
-      {
-        fdrMain.SetBoExecAndResultFalse();
-        FiAppConfig.fiLog?.Error($"{e.Message}");
-        FiAppConfig.fiLog?.Error($"{e.StackTrace}");
-        return fdrMain;
-      }
-
+      // Execution yapılmadı
+      fdrMain.boResult = true;
+      fdrMain.txMessage = "Veritabanı Mevcut.";
+      FiAppConfig.fiLog?.Debug($"Veritabanı mevcut");
+      return fdrMain;
     }
-
-    public Fdr SqlSelectQueryAsDtb(string query, Fkb fkbParams)
+    catch (Exception ex)
     {
-      FiQuery fiQuery = new FiQuery(query, fkbParams);
-      return SqlSelectQueryAsDtb(fiQuery);
+      fdrMain.SetBoExecAndResultFalse();
+      FiAppConfig.fiLog?.Error($"Veritabanı kontrol/oluşturma sırasında bir hata oluştu: {ex.Message}");
+      return fdrMain;
     }
 
-    public Fdr SqlSelectQueryAsDtb(FiQuery fiQuery)
+  }
+
+  public Fdr ExecNonQuery(string query)
+  {
+    FiQuery fiQuery = new FiQuery(query);
+    return ExecNonQuery(fiQuery);
+  }
+
+  public Fdr SqlUpdateQuery(FiQuery fiQuery)
+  {
+    return ExecNonQuery(fiQuery);
+  }
+
+  public Fdr SqlInsertQuery(FiQuery fiQuery)
+  {
+    return ExecNonQuery(fiQuery);
+  }
+
+  public Fdr SqlDeleteQuery(FiQuery fiQuery)
+  {
+    return ExecNonQuery(fiQuery);
+  }
+
+  public Fdr ExecNonQuery(FiQuery fiQuery) //, params ParamItem[] parameters
+  {
+    Fdr fdrMain = new Fdr();
+
+    using SQLiteConnection conn = new SQLiteConnection(this.txConnString);
+    FiAppConfig.fiLog?.Debug(txConnString);
+
+    using SQLiteCommand sqliteCmd = conn.CreateCommand();
+    //Comm.Parameters.Clear();
+    string query = FiQueryUtils.FixSqlProblems(fiQuery.sql);
+    sqliteCmd.CommandText = query;
+    //Comm.CommandType = CommandType.Text;
+
+    try
     {
-      Fdr fdrDtb = new Fdr();
-
-      using SQLiteConnection conn = new SQLiteConnection(txConnString);
-      using SQLiteCommand comm = conn.CreateCommand();
-
-      try
-      {
-        //comm.Parameters.Clear();
-        string query = FiQueryUtils.FixSqlProblems(fiQuery.sql);
-        comm.CommandText = query;
-        //comm.CommandType = CommandType.Text;
-        AddSqlParametersToComm(fiQuery.fkbParams, comm);
-
-        // Microsoft SqLite de yok
-        SQLiteDataAdapter da = new SQLiteDataAdapter(comm);
-        DataTable dt = new DataTable();
-        da.Fill(dt);
-
-        fdrDtb.SetBoExecAndResultTrue();
-        fdrDtb.refValue = dt;
-        fdrDtb.refDtbVal = dt;
-        return fdrDtb;
-      }
-      catch (Exception e)
-      {
-        FiAppConfig.fiLog?.Error(e.Message);
-        fdrDtb.boResult = false;
-        return fdrDtb;
-      }
-
+      conn.Open();
+      AddSqlParametersToComm(fiQuery.fkbParams, sqliteCmd);
+      int lnRowsAffected = sqliteCmd.ExecuteNonQuery();
+      fdrMain.SetBoExecAndResultTrue();
+      fdrMain.lnRowsAffected = lnRowsAffected;
+      return fdrMain;
     }
-    private void AddSqlParametersToComm(Fkb fkbParams, SQLiteCommand comm)
+    catch (Exception e)
     {
-
-      if (fkbParams != null && fkbParams.Count > 0)
-      {
-        comm.Parameters.AddRange(GetArrSqlParams(fkbParams));
-      }
-
+      fdrMain.SetBoExecAndResultFalse();
+      FiAppConfig.fiLog?.Error($"{e.Message}");
+      FiAppConfig.fiLog?.Error($"{e.StackTrace}");
+      return fdrMain;
     }
 
-    public static SQLiteParameter[] GetArrSqlParams(Fkb fkbParams)
+  }
+
+  public Fdr SqlSelectQueryAsDtb(string query, Fkb fkbParams)
+  {
+    FiQuery fiQuery = new FiQuery(query, fkbParams);
+    return SqlSelectQueryAsDtb(fiQuery);
+  }
+
+  public Fdr SqlSelectQueryAsDtb(FiQuery fiQuery)
+  {
+    Fdr fdrMain = new Fdr();
+
+    using SQLiteConnection conn = new SQLiteConnection(txConnString);
+    using SQLiteCommand comm = conn.CreateCommand();
+
+    try
     {
-      if (fkbParams == null) return new List<SQLiteParameter>().ToArray();
+      //comm.Parameters.Clear();
+      string query = FiQueryUtils.FixSqlProblems(fiQuery.sql);
+      comm.CommandText = query;
+      //comm.CommandType = CommandType.Text;
+      AddSqlParametersToComm(fiQuery.fkbParams, comm);
 
-      List<SQLiteParameter> list = new List<SQLiteParameter>();
+      // Microsoft SqLite de yok
+      SQLiteDataAdapter da = new SQLiteDataAdapter(comm);
+      DataTable dt = new DataTable();
+      da.Fill(dt);
 
-      foreach (var fkbItem in fkbParams)
-      {
-        string sqlParamName = "@" + fkbItem.Key;
-        list.Add(new SQLiteParameter(sqlParamName, fkbItem.Value));
-      }
-
-      return list.ToArray();
+      fdrMain.boResult = true;
+      //fdrDtb.refValue = dt;
+      fdrMain.refDtbVal = dt;
+      return fdrMain;
     }
-
-    public DataTable RunProc(string procName) //, params ParamItem[] parameters
+    catch (Exception e)
     {
-      //Comm.Parameters.Clear();
-      //Comm.CommandText = procName;
-      //Comm.CommandType = CommandType.StoredProcedure;
-
-      //if (parameters != null && parameters.Length > 0)
-      //{
-      //	Comm.Parameters.AddRange(ProcessParameters(parameters));
-      //}
-
-      //DataTable dt = new DataTable();
-      //SqlDataAdapter adapter = new SqlDataAdapter(Comm);
-      //adapter.Fill(dt);
-
-      //return dt;
-      return null;
+      FiAppConfig.fiLog?.Error(e.Message);
+      fdrMain.boResult = false;
+      return fdrMain;
     }
 
+  }
+  private void AddSqlParametersToComm(Fkb fkbParams, SQLiteCommand comm)
+  {
 
-  } // end class
+    if (fkbParams != null && fkbParams.Count > 0)
+    {
+      comm.Parameters.AddRange(GetArrSqlParams(fkbParams));
+    }
+
+  }
+
+  public static SQLiteParameter[] GetArrSqlParams(Fkb fkbParams)
+  {
+    if (fkbParams == null) return new List<SQLiteParameter>().ToArray();
+
+    List<SQLiteParameter> list = new List<SQLiteParameter>();
+
+    foreach (var fkbItem in fkbParams)
+    {
+      string sqlParamName = "@" + fkbItem.Key;
+      list.Add(new SQLiteParameter(sqlParamName, fkbItem.Value));
+    }
+
+    return list.ToArray();
+  }
+
+  public DataTable RunProc(string procName) //, params ParamItem[] parameters
+  {
+    //Comm.Parameters.Clear();
+    //Comm.CommandText = procName;
+    //Comm.CommandType = CommandType.StoredProcedure;
+
+    //if (parameters != null && parameters.Length > 0)
+    //{
+    //	Comm.Parameters.AddRange(ProcessParameters(parameters));
+    //}
+
+    //DataTable dt = new DataTable();
+    //SqlDataAdapter adapter = new SqlDataAdapter(Comm);
+    //adapter.Fill(dt);
+
+    //return dt;
+    return null;
+  }
+
+
+} // end class
 
 
 //Comm.Parameters.Clear();
@@ -292,4 +288,3 @@ namespace OrakUtilSqliteCore.FiDbHelper
 //}
 
 // "INSERT INTO SampleTable (Col1, Col2) VALUES('Test Text ', 1); ";
-}
